bd06019a2394ef4189a10e0759a6d394279adb71
Elchonon <ekn400@gmail.com>
2024-12-12 15:56:12 -0500

    lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
-# your code goes here!
+class Anagram:
+    def __init__(self,target):
+        self.target = sorted(target)
+        self.matches = []
+
+    def match(self,list):
+        for option in list:
+            if sorted(option) == self.target:
+                self.matches.append(option)
+        print(self.matches)
+        return self.matches
+
+listen = Anagram("listen")
+listen.match(['enlists', 'google', 'inlets', 'banana'])
+# => ['inlets']
